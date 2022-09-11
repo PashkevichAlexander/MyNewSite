71b12818b01f710aef14e4b1ab93eb1714d05b38
Alexander Pashkevich <pashkevich.a.a@mail.ru>
2022-09-11 22:02:22 +0300

    make and change some files

--- src/main/java/com/mysite/MyNewSite/controllers/GreetingController.java
+++ src/main/java/com/mysite/MyNewSite/controllers/GreetingController.java
@@ -5,20 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class GreetingController {
-
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Model model,
-            HttpServletRequest request
+            Model model
     ) {
-        model.addAttribute("name",name);
-        request.setAttribute("name", name);
+        model.addAttribute("name", name);
         return "greeting";
-
     }
 }
